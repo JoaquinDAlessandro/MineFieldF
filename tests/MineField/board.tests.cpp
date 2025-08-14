3efed860d9f08c1df2121df2a3561c95aacd8097
alteaportilla <agustina.portilla@globant.com>
2025-08-14 16:06:39 -0300

    test board

---
+++ tests/MineField/board.tests.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <MineField/Game.h>
+#include <MineField/Board.h>
+#include <MineField/Coordinate.h>
+
+namespace board::tests
+{
+
+	TEST(Game, should_return_true_if_board_is_set_correctly)
+	{
+        Game game;
+        unsigned int rows = 10;
+        unsigned int columns = 15;
+        game.setBoardSize(10, 15);
+        EXPECT_EQ(game.getBoard().getRows(), rows);
+        EXPECT_EQ(game.getBoard().getColumns(), columns);
+
+    }
+
+    TEST(Game, should_return_nullptr_when_accesing_coordinate_if_board_is_not_initialized)
+    {
+        Game game;
+        Board board;
+        int row = 10;
+        int column = 10;
+        Coordinate const* coord = board.getCoordinate(row, column);
+        EXPECT_EQ(coord, nullptr);
+    }
+	
+}
